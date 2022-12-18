27359f8556adce28ffe5734761ca35de5a210f1e
deanSyue <c1318131801@gmail.com>
2022-12-18 11:53:14 +0800

    modify: model:calender的foreignKey改為字串

--- models/calender.js
+++ models/calender.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Calender.hasMany(models.Attendance, { foreignKey: date })
+      Calender.hasMany(models.Attendance, { foreignKey: 'date' })
     }
   }
   Calender.init({
