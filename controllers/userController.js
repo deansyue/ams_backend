50667c8428a29e12974e3b5c2690e8e50ff7906b
deanSyue <c1318131801@gmail.com>
2022-12-25 22:03:45 +0800

    feat: 新增修改密碼路由

---
+++ controllers/userController.js
@@ -0,0 +1,59 @@
+const { User } = require('../models')
+const bcrypt = require('bcryptjs')
+const helpers = require('../helpers/auth-helper')
+
+const userController = {
+  changePassword: async (req, res, next) => {
+    try {
+      const { oldPassword, newPassword, confirmPassword } = req.body
+      const userId = helpers.getUser(req).id
+
+      // 確認欄位沒有空白
+      if (!oldPassword || !newPassword || !confirmPassword) {
+        return res.json({
+          status: 'error',
+          message: '欄位不可空白'
+        })
+      }
+
+      // 確認新密碼與密碼確認是否一致
+      if (newPassword !== confirmPassword) {
+        return res.json({
+          status: 'error',
+          message: '新密碼與密碼確認不一致，請重新輸入'
+        })
+      }
+
+      const user = await User.findByPk(userId)
+
+      // 確認是否有該使用者
+      if (!user) {
+        return res.json({
+          status: 'error',
+          message: '無此使用者,請重新輸入'
+        })
+      }
+
+      const pasIsCorrect = await bcrypt.compare(oldPassword, user.password)
+
+      // 確認舊密碼是否輸入正確
+      if (!pasIsCorrect) {
+        return res.json({
+          status: 'error',
+          message: '舊密碼輸入錯誤,請重新確認'
+        })
+      }
+
+      await user.update({ password: bcrypt.hashSync(newPassword, bcrypt.genSaltSync(10)) })
+
+      return res.json({
+        status: 'success',
+        message: '密碼變更成功,請重新登入'
+      })
+    } catch (err) {
+      next(err)
+    }
+  }
+}
+
+module.exports = userController
--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,16 @@ const { errorHandler } = require('../middleware/error-handler')
 const { authenticated, authenticatedUser } = require('../middleware/auth')
 const signController = require('../controllers/signController')
 const checkController = require('../controllers/checkController')
+const userController = require('../controllers/userController')
 
 // 登入路由
 router.route('/signin').post(signController.signIn)
 
 // 打卡路由
 router.route('/checkin/:attMode').all(authenticated, authenticatedUser).post(checkController.checkIn)
+
+// 修改密碼路由
+router.route('/users/changePassword').all(authenticated, authenticatedUser).post(userController.changePassword)
 
 router.use('/', errorHandler)
 
