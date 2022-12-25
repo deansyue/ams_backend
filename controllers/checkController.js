91ecc585a7ea059ee999fee1edca6281e12e89d9
deanSyue <c1318131801@gmail.com>
2022-12-25 12:06:00 +0800

    modify: 安裝moment套件,將date物件以moment改寫，並將時間以台北時區計算

--- controllers/checkController.js
+++ controllers/checkController.js
@@ -1,3 +1,4 @@
+const moment = require('moment')
 const { Attendance } = require('../models')
 const helpers = require('../helpers/auth-helper')
 
@@ -5,21 +6,21 @@ const checkController = {
   checkIn: async (req, res, next) => {
     try {
       const attMode = Number(req.params.attMode)
-      let { checkTime } = req.body
+      let { checkTime } = req.body // timestamp字串
       const userId = helpers.getUser(req).id
-      // 將前端傳來的毫秒時間轉換為日期格式
-      checkTime = new Date(Number(checkTime))
-      const checkHour = checkTime.getHours()
-      let checkDate = new Date(checkTime)
+      const timeZone = 'Asia/Taipei'
+
+      // 將前端傳回的字串，轉為moment格式進行計算
+      checkTime = moment.tz(checkTime, timeZone)
+      const checkHour = Number(checkTime.format('H'))
+      let checkDate = moment.tz(checkTime, timeZone)
 
       // 當打卡時間為5點以前，打卡登入的日期為前一天
       // 打卡時間為 當天5:00 ~ 隔天4:59為一天
       if (checkHour < 5) {
-        checkDate = checkDate.setDate(checkDate.getDate() - 1)
-        checkDate = new Date(checkDate)
-        checkDate = checkDate.toLocaleDateString()
+        checkDate = checkDate.subtract(1, 'd').format('YYYY/MM/DD')
       } else {
-        checkDate = checkDate.toLocaleDateString()
+        checkDate = checkDate.format('YYYY/MM/DD')
       }
 
       const attData = await Attendance.findOne({ where: { UserId: userId, date: checkDate } })
@@ -44,10 +45,16 @@ const checkController = {
 
         // 有打卡資料時，視為下班>判斷工作時數是否達到8小時，並更新下班時間及出缺勤fg
       } else {
+        const workTime = moment.tz(attData.workTime, timeZone)
+
         //判斷與上班日期相比，若下班打卡日期為隔天，下班打卡時數要加24小時
-        const compareHour = attData.workTime.toLocaleDateString() === checkTime.toLocaleDateString() ? checkHour : checkHour + 24
-        // 下班小時-上班小時 > 8小時，出缺勤fg=1, 否則為0
-        const attFg = (compareHour - attData.workTime.getHours()) >= 8 ? 0 : 1
+        // 將上班時間與下班時間 換算成分鐘
+        let compareMinutes = workTime.format('YYYYMMDD') === checkTime.format('YYYYMMDD') ? checkHour : checkHour + 24
+        compareMinutes = (compareMinutes*60) + Number(checkTime.format('mm'))
+        const workMinutes = (Number(workTime.format('H')) * 60) + Number(workTime.format('mm'))
+
+        // 下班時間-上班間 >= 480分，出缺勤fg=1, 否則為0
+        const attFg = (compareMinutes - workMinutes) >= (8 * 60) ? 0 : 1
 
         await attData.update({ offTime: checkTime, attFg })
 
