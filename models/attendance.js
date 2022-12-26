d87bafcb339532fba278fd00c0ba41bb7c49a68a
deanSyue <c1318131801@gmail.com>
2022-12-26 21:54:23 +0800

    modify: 刪除attendance的attMode欄位

--- models/attendance.js
+++ models/attendance.js
@@ -25,7 +25,6 @@ module.exports = (sequelize, DataTypes) => {
     },
     workTime: DataTypes.DATE,
     offTime: DataTypes.DATE,
-    attMode: DataTypes.INTEGER,
     workGps: DataTypes.STRING,
     offGps: DataTypes.STRING,
     attFg: DataTypes.INTEGER
